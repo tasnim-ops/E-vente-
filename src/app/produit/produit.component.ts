80b396692cf24420bec89e049270b5217d8f3a28
tasnim <hajji.tas520@gmail.com>
2023-05-04 23:01:23 +0100

    initial commit

---
+++ src/app/produit/produit.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css']
+})
+export class ProduitComponent {
+  nom:String="med ali";
+  adresse:String="sfax";
+  age:number=25;
+
+}
